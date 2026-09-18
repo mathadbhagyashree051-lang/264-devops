6e75a28c16f195ca1f5aab6a6559aa5a7d5789ed
Bhagyashree <mathadbhagyashree051@gmail.com>
2026-09-18 11:13:58 +0530

    add swapping program pgm3

---
+++ pgm3/swapping.py
@@ -0,0 +1,15 @@
+# Python program to swap two numbers
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("\nBefore swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+
+# Swapping
+num1, num2 = num2, num1
+
+print("\nAfter swapping:")
+print("First number:", num1)
+print("Second number:", num2)
